61eb5412410424b672314697b7be03dc473d9c18
winceweb <winceweb@gmail.com>
2017-06-09 14:28:41 +0200

    modif du welcome TEST

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    {{ $message }}
+                    TEST TEST TEST TEST {{ $message }}
                 </div>
 
                 <div class="links">
