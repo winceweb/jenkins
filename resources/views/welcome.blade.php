ddf51199b42cc5771a692607a72a17912b9284e0
winceweb <winceweb@gmail.com>
2017-06-09 15:14:10 +0200

    houhouhou

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    ZERZERZERZERZE {{ $message }}
+                    HOUHOUHOUHOU {{ $message }}
                 </div>
 
                 <div class="links">
