61a70f400a358a7a913144fc2f1c1ee91224ee25
winceweb <winceweb@gmail.com>
2017-06-09 08:17:20 +0000

    change welcome title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-			Fragile                    
+			le TEST ULTIME 
                 </div>
 
                 <div class="links">
